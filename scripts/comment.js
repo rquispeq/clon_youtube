ccb1b149e373a917d73760688929ef8fdf285f99
Rony Quispe <oghmand@gmail.com>
2021-08-02 18:12:24 -0500

    Componente de comments con recursos de la clase.

---
+++ scripts/comment.js
@@ -0,0 +1,18 @@
+import {commentTemplate} from "/templates/comment.js";
+import {commentData} from "/helpers/get-comment-data.js";
+
+const handleRenderComments = () => {
+  const commentsGrid = document.getElementById('comments-grid')
+
+  if (!commentsGrid) return
+
+  for (let i = 1; i <= 30; i++) {
+    const element = document.createElement('article')
+    const data = commentData()
+    element.classList.add('comment', 'flex')
+    element.innerHTML = commentTemplate(data)
+    commentsGrid.appendChild(element)
+  }
+}
+
+handleRenderComments()
